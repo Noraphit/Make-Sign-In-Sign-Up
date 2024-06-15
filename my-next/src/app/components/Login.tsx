3f1c757641f800373b736792b45acc8644ab93e1
noraphit <noraphit.p@gmail.com>
2024-06-15 17:22:08 +0700

    Clone repo and create next project

---
+++ my-next/src/app/components/Login.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type Props = {}
+
+const Login = (props: Props) => {
+  return (
+    <>
+      <h1>lol</h1>
+    </>
+  );
+};
+
+export default Login;
---
+++ my-next/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Login from "./components/Login";
+
+export default function Home() {
+  return (
+    <>
+    <main>
+      <Login />
+    </main>
+    </>
+  );
+}
