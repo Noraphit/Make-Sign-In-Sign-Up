f8b3abc71e315cc86fc7fcdcf2404df27d8b16bf
noraphit <noraphit.p@gmail.com>
2024-06-15 17:41:19 +0700

    Create UI and added images

--- my-next/src/app/components/Login.tsx
+++ my-next/src/app/components/Login.tsx
@@ -1,11 +1,38 @@
+"use client";
 import React from 'react';
+import Image from 'next/image';
 
 type Props = {}
 
 const Login = (props: Props) => {
   return (
     <>
-      <h1>lol</h1>
+      <div className="container">
+        <div className="header">
+          <div className="text">Sign Up</div>
+        </div>
+      </div>
+
+      <div className="inputs">
+        <div className="input">
+          <Image
+            src="/images/email.png"
+            width={20}
+            height={20}
+            alt="x"
+            className="cursor-pointer"
+          />
+          <input type="text" />
+        </div>
+
+        <div className="input">
+          <input type="email" />
+        </div>
+
+        <div className="input">
+          <input type="password" />
+        </div>
+      </div>
     </>
   );
 };
