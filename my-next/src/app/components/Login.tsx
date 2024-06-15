c4f93b306d84910ba56aa403fbfe1c899027836a
noraphit <noraphit.p@gmail.com>
2024-06-15 18:09:57 +0700

    Update UI interface

--- my-next/src/app/components/Login.tsx
+++ my-next/src/app/components/Login.tsx
@@ -1,36 +1,62 @@
 "use client";
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 
 type Props = {}
 
 const Login = (props: Props) => {
+  const [action, setAction] = useState("Sign");
   return (
     <>
-      <div className="container">
-        <div className="header">
-          <div className="text">Sign Up</div>
-        </div>
-      </div>
-
-      <div className="inputs">
-        <div className="input">
-          <Image
-            src="/images/email.png"
-            width={20}
-            height={20}
-            alt="x"
-            className="cursor-pointer"
-          />
-          <input type="text" />
+      <div className="container flex flex-col m-auto mt-[200px] bg-white pb-[30px] w-[600px]">
+        <div className="header flex flex-col items-center gap-[9px] w-[100%] mt-[30px]">
+          <div className="text text-[#3c009d] text-[48px] font-[700]">Sign Up</div>
+          <div className="underline w-[91px] h-[6px] bg-[#3c009d] round-[9px]"></div>
         </div>
 
-        <div className="input">
-          <input type="email" />
-        </div>
+        <div className="inputs mt-[55px] flex flex-col gap-[25px]">
+          <div className="input flex items-center m-auto w-[480px] h-[80px] bg-[#eaeaea] rounded-[6px]">
+            <Image
+              src="/images/person.png"
+              width={25}
+              height={25}
+              alt="x"
+              className="mx-[30px]"
+            />
+            <input type="text"
+              className="h-[50px] w-[400px] bg-transparent border-none outline-none text-[19px] text-[#797979]" placeholder="Name"/>
+          </div>
 
-        <div className="input">
-          <input type="password" />
+          <div className="input input flex items-center m-auto w-[480px] h-[80px] bg-[#eaeaea] rounded-[6px]">
+            <Image
+              src="/images/email.png"
+              width={25}
+              height={25}
+              alt="x"
+              className="mx-[30px]"
+            />
+            <input type="email"
+              className="h-[50px] w-[400px] bg-transparent border-none outline-none text-[19px] text-[#797979]" placeholder="Email" />
+          </div>
+
+          <div className="input input flex items-center m-auto w-[480px] h-[80px] bg-[#eaeaea] rounded-[6px]">
+            <Image
+              src="/images/password.png"
+              width={25}
+              height={25}
+              alt="x"
+              className="mx-[30px]"
+            />
+            <input type="password"
+              className="h-[50px] w-[400px] bg-transparent border-none outline-none text-[19px] text-[#797979]" placeholder="Password" />
+          </div>
+
+          <div className="forgot-password pl-[62px] mt-[27px] text-[#797979] text-[18px]">Forgot Password? <span className="text-[#4c00b4] cursor-pointer">Click Here!</span></div>
+
+          <div className="submit-container flex gap-[30px] my-[20px] mx-auto">
+            <div className="submit flex justify-center items-center w-[220px] h-[59px] text-[#fff] bg-[#4c00b4] rounded-[50px] text-[#19px] font-[700] cursor-pointer">Sign Up</div>
+            <div className="submit submit flex justify-center items-center w-[220px] h-[59px] text-[#fff] bg-[#4c00b4] rounded-[50px] text-[#19px] font-[700] cursor-pointer">Login</div>
+          </div>
         </div>
       </div>
     </>
